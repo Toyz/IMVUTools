5c64e78af72d782ecf13287219e221c0501160da
toyz <toyzz@live.com>
2023-04-07 23:50:59 -0700

    added the ability to patch the checksum.txt from the tool

--- imvu.ps1
+++ imvu.ps1
@@ -1,17 +1,19 @@
-$imvuFolderDefault = [IO.Path]::Combine($env:APPDATA, "IMVUClient", "library", "imvu");
+$imvuFolderDefault = [IO.Path]::Combine($env:APPDATA, "IMVUClient", "library");
 
 while ($true) {
     $mode = Read-Host -Prompt 'Build, Decompile or Extract'
 
     switch ($mode.ToLower()) {
         "extract" {
-            $imvuLibPath = [IO.Path]::Combine($imvuFolderDefault, "..", "..", "library.zip")
-            $imvuDest = [IO.Path]::Combine($imvuFolderDefault, "..")
-            Expand-Archive -LiteralPath $imvuLibPath -DestinationPath $imvuDest
+            $imvuLibPath = [IO.Path]::Combine($imvuFolderDefault, "..", "library.zip")
+            $imvuDest = [IO.Path]::Combine($imvuFolderDefault, "..", "library")
+            Expand-Archive -LiteralPath $imvuLibPath -DestinationPath $imvuDest -Force
             break;
         }
         "build" {
-            $files = Get-ChildItem $imvuFolderDefault -Recurse -Filter *.py -Exclude *.pyo
+            $imvuLibPath = [IO.Path]::Combine($imvuFolderDefault, "imvu")
+
+            $files = Get-ChildItem $imvuLibPath -Recurse -Filter *.py -Exclude *.pyo
             foreach ($file in $files) { 
                 Write-Output $file.FullName
                 C:\Python27\python.exe -O -m compileall $file.FullName
@@ -20,6 +22,16 @@ while ($true) {
         }
         "decompile" {
             $files = Get-ChildItem $imvuFolderDefault -Recurse -Filter *.pyo -Exclude *.pyo_dis
+            foreach ($file in $files) { 
+                Write-Output $file.DirectoryName
+                $outputFile = [IO.Path]::Combine($file.DirectoryName, $file.BaseName + ".py")
+                C:\Python27\Scripts\uncompyle6.exe -o $outputFile $file.FullName
+            }
+        }
+        "decompile-imvu" {
+            $imvuLibPath = [IO.Path]::Combine($imvuFolderDefault, "imvu")
+
+            $files = Get-ChildItem $imvuLibPath -Recurse -Filter *.pyo -Exclude *.pyo_dis
             foreach ($file in $files) { 
                 Write-Output $file.DirectoryName
                 $outputFile = [IO.Path]::Combine($file.DirectoryName, $file.BaseName + ".py")
@@ -44,6 +56,18 @@ while ($true) {
         }
         "exit" {
             exit
+        }
+        "patch" {
+            $imvuLibPath = [IO.Path]::Combine($imvuFolderDefault, "..", "checksum.txt")
+
+            $lines = Get-Content $imvuLibPath
+            # remove line that contains "library.zip"
+            $lines = $lines | Where-Object { $_ -notlike "*library.zip*" }
+
+            # save the lines back to the file  
+            $lines | Set-Content $imvuLibPath
+
+            Write-Output "Patched checksum.txt"
         }
         default {
             Write-Host "Build, Decompile, or Extract supported only"
